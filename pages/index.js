9f1e2c8c764fe68ace45dffc6fab3c64cf3fc1e4
Heeeera <chr5487@naver.com>
2022-07-17 16:10:51 +0900

    Fix main layout, remove visitors

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
 import Button from '../components/Button';
-
+import utilStyles from '../styles/utils.module.css';
+import gitLogo from '../public/github.png';
 export default function Home() {
-  const menu = ["💁‍♀️기본 정보", "🎶좋아하는 것", "📝방명록"];
-  const url = ["/information", "/favorite", "/visitors"];
+  const menu = ["💁‍♀️기본 정보", "🎶좋아하는 것"];
+  const url = ["/information", "/favorite"];
 
   return (
     <Layout home>
@@ -16,6 +18,10 @@ export default function Home() {
         <p>안녕하세요. 저의 사이트에 오신 것을 환영합니다.</p>
         <p>👋👋👋</p>
         <div className={utilStyles.button}>{menu.map((data, idx) => <Button key={idx} name={data} url={url[idx]}/>)}</div>
+        {/* <div style={{fontSize:'15px', padding: '20px'}}>📧 heeeeera5487@gmail.com</div> */}
+        <Link href="https://github.com/Heeeera" passHref>
+          <a><Image src={gitLogo} height='49' width='49' alt="git"/></a>
+        </Link>
       </section>
     </Layout>
   );

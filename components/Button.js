abd999fc45a80d02f37d96e886fd418459f22da8
Heeeera <chr5487@naver.com>
2022-07-27 17:44:32 +0900

    Add styled-components in main page

--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,25 @@
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    width: 10rem;
+    height: 3rem;
+    background-color: white;
+    border: 2px solid #FFDEB4;
+    border-radius: 10px;
+    margin: 1.25rem 0.625rem 0.652rem;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    &:hover {
+        border: 3px solid #B2A4FF;
+    } 
+`;
 
 function Button({name, url}) {
-
     return (
         <Link href={url} passHref>
-            <button className="w-40 h-12 bg-white border-2 border-solid border-gray-500 rounded-lg mt-5 mb-2.5 mx-2.5 text-sm" >{name}</button>
+            {/* <button className="w-40 h-12 bg-white border-2 border-solid border-gray-500 rounded-lg mt-5 mb-2.5 mx-2.5 text-sm" >{name}</button> */}
+            <StyledButton>{name}</StyledButton>
         </Link>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,62 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import styled from 'styled-components';
 import Layout, { siteTitle } from '../components/layout';
 import Button from '../components/Button';
 import gitLogo from '../public/github.png';
+
+export const Container = styled.div`
+  background-color: #FFDEB4;
+  font-family: "Arial", sans-serif;
+`;
+
+const ContentContainer = styled.div`
+  background-color: #FFB4B4;
+  border-radius: 30px;
+  min-height: 30vh;
+  padding: 0.5rem;
+  max-width: 600px;
+  margin: 0 auto;
+  @media screen and (max-width: 500px){
+    max-width: 80vw;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 500px){
+    max-width: 80vw;
+    flex-direction: column;
+`;
 
 export default function Home() {
   const menu = ["💁‍♀️About Me", "🎶Favorites","💖I'm seoyoung"];
   const url = ["/aboutMe", "/favorite", "/seoyoung"];
 
   return (
-    <div className="flex items-center min-h-screen">
-      <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className="flex flex-col items-center text-lg my-4">
-          <p className="mb-5">Hello, Welcome to my home!</p>
-          <p>👋👋👋</p>
-          <div className="pb-5">{menu.map((data, idx) => <Button key={idx} name={data} url={url[idx]}/>)}</div>
-          <Link href="https://github.com/Heeeera" passHref>
-            <a className="pt-3"><Image src={gitLogo} height='49' width='49' alt="git"/></a>
-          </Link>
-        </section>
-      </Layout>
-    </div>
+    <Container>
+      <div className="flex items-center min-h-screen">
+        <Layout home>
+          <Head>
+            <title>{siteTitle}</title>
+          </Head>
+          <ContentContainer>
+            <section className="flex flex-col items-center text-lg my-4">
+              <p className="mb-5">Hello, Welcome to my home!</p>
+              <p>👋👋👋</p>
+              <ButtonContainer>{menu.map((data, idx) => <Button key={idx} name={data} url={url[idx]}/>)}</ButtonContainer>
+            </section>
+          </ContentContainer>
+          <div className="flex flex-col items-center">
+            <Link href="https://github.com/Heeeera" passHref>
+              <a className="pt-3"><Image src={gitLogo} height='49' width='49' alt="git"/></a>
+            </Link>
+          </div>
+        </Layout>
+      </div>
+    </Container>
   );
 }
